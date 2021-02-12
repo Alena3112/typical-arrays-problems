d738d5f17cfc1ecd7843c74b4341130725ff02d7
Alena3112 <61665062+Alena3112@users.noreply.github.com>
2021-02-12 23:29:11 +0300

    refactor: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
-  return 0;
+ exports.min = function min (array){
+    array = typeof array !== 'undefined' ? array : [0];
+    if (array.length === 0) return 0;
+    else {return  Math.min.apply(null, array);}
 }
 
-exports.max = function max (array) {
-  return 0;
+   exports.max = function max (array) {
+    array = typeof array !== 'undefined' ? array : [0];
+    if (array.length === 0) return 0;
+    else {return  Math.max.apply(null, array);}
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+   exports.avg = function avg (array) {
+  
+    array = typeof array !== 'undefined' ? array : [0];
+    var sum = 0;
+for (var i = 0; i < array.length; i++) 
+{sum += array[i];}
+
+if (array.length === 0) return 0;
+else {return sum/array.length}
+    }
